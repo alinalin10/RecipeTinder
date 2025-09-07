c576ed5232944cde7b8b725eb7ca141082c8e163
Alina Lin <alinalin@Alinas-MacBook-Air-2.local>
2025-09-07 14:44:22 -0400

    set up app

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next)=> {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to RecipeTinder API'})
+})
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for reqs
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
