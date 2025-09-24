b376f914725da609a2b6f64af421a8ef0ac1962c
Alina Lin <alinalin@Alinas-MacBook-Air-2.local>
2025-09-24 17:54:29 -0400

    added shared recipes tab to navbar

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -16,6 +16,7 @@ export default function NavBar() {
     <div className="nav-actions"></div>
       <div className="links">
         <Link href="/about">About</Link>
+        <Link href="/recipes">Share Recipes</Link>
         <Link href="/recipes">My Recipes</Link>
         <Link href="/login">Login</Link>
       </div>
