b52a9288511403bdc756b036494d3d37273918e9
vaibhavw30 <vaibhav.wudaru@gmail.com>
2025-09-30 19:18:17 -0400

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,20 @@
 const express = require('express')
 
 //controller function
-const {signupUser, loginUser} = require('../controllers/userController')
+const { signupUser, loginUser } = require('../controllers/userController')
 
 //makes an instance of the express router
 const router = express.Router()
-
-module.exports = router
 
 //login route
 router.post('/login', loginUser)
 
 //signup route
 router.post('/signup', signupUser)
+
+// debug ping route to verify routing and server reachability
+router.get('/ping', (req, res) => {
+	res.json({ ok: true, path: req.path, method: req.method })
+})
 
 module.exports = router
