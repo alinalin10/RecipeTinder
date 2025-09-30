8f5612569fa18cd02b70951004d26aaf0f1fba95
semilore <semilore@Akinbos-MacBook-Air.local>
2025-09-30 16:28:46 -0400

    recipe page

--- frontend/src/app/(main)/about/page.tsx
+++ frontend/src/app/(main)/about/page.tsx
@@ -1,11 +1,13 @@
-import MakeRecipe from "../../components/Makerecipe";
-
+import Makerecipe from "../../components/Makerecipe";
+import Recipe from "../../components/Recipe";
 
 const About = () => {
     return (
         <div>
             <h1>"About Recipe Tinder</h1>
             <p>Recipe Tinder is a fun and interactive way to discover new recipes. Swipe right to save your favorites!</p>
+            {/* <Makerecipe /> */}
+            <Recipe />
         </div>
     );
 };
