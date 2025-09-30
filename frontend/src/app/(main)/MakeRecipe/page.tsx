c31c4705d2af827a3cd4deecf1c3652262f6f5c1
semilore <semilore@Akinbos-MacBook-Air.local>
2025-09-30 18:56:25 -0400

    recipes

---
+++ frontend/src/app/(main)/MakeRecipe/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Makerecipe from '../components/Makerecipe'
+import { Link } from 'react-router'
+
+
+const page = () => {
+  return (
+    <div>
+        <h1>Make a recipe</h1>
+        <Makerecipe />
+    </div>
+  )
+}
+
+export default page
