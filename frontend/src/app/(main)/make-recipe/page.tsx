bef3c152bfa94172b126d3a06017a8e53eac01aa
Alina Lin <alinalin@Alinas-MacBook-Air-2.local>
2025-09-30 22:08:55 -0400

    refactored code

--- frontend/src/app/(main)/make-recipe/page.tsx
+++ frontend/src/app/(main)/make-recipe/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Makerecipe from '../components/Makerecipe'
-import { Link } from 'react-router'
+import Makerecipe from '../../components/Makerecipe'
 
 
 const page = () => {
   return (
     <div>
-        <h1>Make a recipe</h1>
         <Makerecipe />
     </div>
   )
