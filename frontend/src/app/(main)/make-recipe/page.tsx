fc81f6807435c8cd55d0392f2d4e9f2e705539c1
semilore <semilore@Akinbos-MacBook-Air.local>
2025-10-17 17:47:25 -0400

    update

--- frontend/src/app/(main)/make-recipe/page.tsx
+++ frontend/src/app/(main)/make-recipe/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+<<<<<<< Updated upstream:frontend/src/app/(main)/make-recipe/page.tsx
 import Makerecipe from '../../components/Makerecipe'
+=======
+import Makerecipe from '../components/Makerecipe'
+>>>>>>> Stashed changes:frontend/src/app/(main)/MakeRecipe/page.tsx
 
 
 const page = () => {
   return (
+<<<<<<< Updated upstream:frontend/src/app/(main)/make-recipe/page.tsx
     <div>
+=======
+    <div className='bg-white'>
+        <h1>Make a recipe</h1>
+>>>>>>> Stashed changes:frontend/src/app/(main)/MakeRecipe/page.tsx
         <Makerecipe />
     </div>
   )
