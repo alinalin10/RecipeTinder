178e84750e107e12a17f1ba25e4636e4ae951ade
Alam <aalam64@gatech.edu>
2025-10-28 18:15:31 -0400

    Swipe functionality

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user')
 const userRecipeRoutes = require('./routes/User-Recipe-Routes');
+const swipeRoutes = require('./routes/swipe');
 
 /*debug for .env secret JWT signature not being held secret
 
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/user', userRoutes)
 app.use('/api/userrecipes', userRecipeRoutes);
+app.use('/api/recipes', swipeRoutes);
 
 // Basic test route
 app.get('/', (req, res) => {
